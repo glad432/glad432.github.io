96df2606561b68f80fbcc096ef9b2363d3c3f691
Glad <120590157+glad432@users.noreply.github.com>
2023-10-28 19:17:20 +0530

    main

--- js/script.js
+++ js/script.js
@@ -151,10 +151,10 @@ function setupFileInput() {
 		if (droppedFile) {
 			if (droppedFile.name.toLowerCase().endsWith('.py')) {
 				fileNameDisplay.textContent = droppedFile.name;
-				fileNameDisplay.classList.add('font-bold', 'bg-green-500', 'text-white', 'py-1', 'px-2', 'rounded');
+				fileNameDisplay.classList.add('select-none', 'font-bold', 'bg-green-500', 'text-white', 'py-1', 'px-2', 'rounded');
 				handleFile(droppedFile);
 			} else {
-				errorMessage.classList.add('font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
+				errorMessage.classList.add('select-none', 'font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
 				errorMessage.textContent = "Invalid file format. Please select a .py file.";
 
 				fileNameDisplay.textContent = '';
@@ -179,10 +179,10 @@ function setupFileInput() {
 
 			reader.readAsText(file);
 		} else {
-			errorMessage.classList.add('font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
+			errorMessage.classList.add('select-none', 'font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
 			errorMessage.textContent = "File size exceeds 2MB. Please select a smaller file.";
 			fileNameDisplay.textContent = '';
-			fileNameDisplay.classList.remove('font-bold', 'bg-green-500', 'text-white', 'py-1', 'px-2', 'rounded');
+			fileNameDisplay.classList.remove('select-none', 'font-bold', 'bg-green-500', 'text-white', 'py-1', 'px-2', 'rounded');
 			fileInput.value = '';
 		}
 	}
@@ -220,7 +220,7 @@ function updateLineCount() {
 	var text = sourceEditor.getValue();
 	var textSizeInBytes = new TextEncoder().encode(text).length;
 
-	document.getElementById("line-count").textContent = "Line Count: " + lineCount;
+	document.getElementById("line-count").textContent = `Line Count: ${lineCount}`;
 	document.getElementById("text-size").textContent = (textSizeInBytes / 1024).toFixed(3) + " kB";
 }
 
@@ -238,7 +238,7 @@ var lineCountDiv = document.getElementById("line-count-out");
 
 function updateLineCount_out() {
 	var lineCount_out = minifiedEditor.lineCount();
-	lineCountDiv.textContent = "Line Count: " + lineCount_out;
+	lineCountDiv.textContent = `Line Count: ${lineCount_out}`;
 }
 
 minifiedEditor.on("change", updateLineCount_out);
@@ -301,8 +301,8 @@ function initializeMinifier() {
 
 		minifiedEditor.setValue('');
 		const minifiedSizeSpan = document.getElementById('minified-size');
-		const load = '<svg aria-hidden="true" role="status" class="inline font-bold w-4 h-4 mr-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/></svg>';
-		minifiedSizeSpan.innerHTML = load + 'Loading....';
+		const load = '<svg class="select-none inline font-bold w-4 h-4 mr-1 text-white animate-spin" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#fff"><g fill="none" fill-rule="evenodd"><g transform="translate(1 1)" stroke-width="2"><circle stroke-opacity=".5" cx="18" cy="18" r="18"/><path d="M36 18c0-9.94-8.06-18-18-18"><animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="1s" repeatCount="indefinite"/></path></g></g></svg>';
+		minifiedSizeSpan.innerHTML = `${load} Loading....`;
 
 		try {
 			const response = await fetch(api_url + '?' + build_query(), {
@@ -316,7 +316,7 @@ function initializeMinifier() {
 			if (response.ok) {
 				const minified = await response.text();
 				minifiedEditor.setValue(minified);
-				minifiedSizeSpan.innerHTML = (minified.length / 1024).toFixed(3) + ' kB';
+				minifiedSizeSpan.innerHTML = `${(minified.length / 1024).toFixed(3)}  kB`;
 				copyButton.disabled = false;
 			} else {
 				copyButton.disabled = true;
@@ -340,7 +340,7 @@ function initializeMinifier() {
 
 	function updateSourceSize() {
 		const sourceSizeSpan = document.getElementById('text-size');
-		sourceSizeSpan.innerHTML = (sourceTextArea.value.length / 1024).toFixed(3) + '  kB';
+		sourceSizeSpan.innerHTML = `${(sourceTextArea.value.length / 1024).toFixed(3)}  kB`;
 
 	}
 
@@ -379,9 +379,9 @@ document.getElementById("rm").addEventListener("click", function() {
 
 	generateButton.disabled = true;
 
-	fileNameDisplay.classList.remove('font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
+	fileNameDisplay.classList.remove('select-none', 'font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
 
-	errorMessage.classList.remove('font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
+	errorMessage.classList.remove('select-none', 'font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
 
 	errorMessage.textContent = '';
 
@@ -404,9 +404,9 @@ document.getElementById("rm0").addEventListener("click", function() {
 
 	generateButton.disabled = true;
 
-	fileNameDisplay.classList.remove('font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
+	fileNameDisplay.classList.remove('select-none', 'font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
 
-	errorMessage.classList.remove('font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
+	errorMessage.classList.remove('select-none', 'font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
 
 	errorMessage.textContent = '';
 
@@ -427,10 +427,11 @@ initializeMinifier();
 var contentVisible1 = false;
 
 function toggleContent1() {
-	var content = document.querySelector('.content-ll');
-	if (content) {
-		content.style.display = contentVisible1 ? 'none' : 'block';
-		contentVisible1 = !contentVisible1;
+	var contentDiv = document.querySelector('.content-ll');
+	if (contentDiv.style.maxHeight) {
+		contentDiv.style.maxHeight = null;
+	} else {
+		contentDiv.style.maxHeight = contentDiv.scrollHeight + 'px';
 	}
 }
 
