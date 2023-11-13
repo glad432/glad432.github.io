acc25074432f88367f0890e06d00983e89806ff9
gladw-in <89470287+gladw-in@users.noreply.github.com>
2023-11-13 10:30:22 +0530

    main

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
+const minifyButton = document.getElementById('minify');
+const copyButton = document.getElementById('copy');
 const textElement = document.getElementById("text");
 const cursorElement = document.getElementById("cursor");
-const orderedListElement = document.getElementById("orderedList");
+const errorMessage = document.getElementById('errmsg');
+const minifiedSizeElement = document.getElementById("minified-size");
+const generateButton = document.getElementById('dw');
+const dropArea = document.getElementById('dropArea');
+const fileInput = document.getElementById('fileInput');
+const fileNameDisplay = document.getElementById('fileNameDisplay');
+const minifiedSizeSpan = document.getElementById('minified-size');
+var preservedGlobalsInput = document.getElementById('preserve_globals');
+var contentDiv = document.querySelector('.content-ll');
+var content = document.querySelector('.content-ll');
+var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+let sentenceIndex = 0;
+let charIndex = 0;
+let isTyping = true;
+var contentVisible = false;
+var contentVisible1 = false;
+var words = [
+	'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif',
+	'else', 'except', 'False', 'finally', 'for', 'from', 'global', 'if', 'import',
+	'in', 'is', 'lambda', 'None', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return',
+	'True', 'try', 'while', 'with', 'yield', 'abs', 'all', 'any', 'ascii', 'bin',
+	'bool', 'bytearray', 'bytes', 'callable', 'chr', 'classmethod', 'compile', 'complex',
+	'delattr', 'dict', 'dir', 'divmod', 'enumerate', 'eval', 'exec', 'filter', 'float',
+	'format', 'frozenset', 'getattr', 'globals', 'hasattr', 'hash', 'help', 'hex', 'id',
+	'input', 'int', 'isinstance', 'issubclass', 'iter', 'len', 'list', 'locals', 'map',
+	'max', 'memoryview', 'min', 'next', 'object', 'oct', 'open', 'ord', 'pow', 'print',
+	'property', 'range', 'repr', 'reversed', 'round', 'set', 'setattr', 'slice', 'sorted',
+	'staticmethod', 'str', 'sum', 'super', 'tuple', 'type', 'vars', 'zip', 'exec', 'Ellipsis',
+	'NotImplemented',
+]
 
 const sentences = [
 	"A Python minifier is a tool used to shrink Python code size by eliminating unnecessary elements like white spaces, comments, and line breaks.",
@@ -17,10 +48,6 @@ function shuffleArray(array) {
 		[array[i], array[j]] = [array[j], array[i]];
 	}
 }
-
-let sentenceIndex = 0;
-let charIndex = 0;
-let isTyping = true;
 
 function type() {
 	if (charIndex < sentences[sentenceIndex].length) {
@@ -54,7 +81,7 @@ function startClearingText() {
 
 function showOrderedList() {
 	const bbbtn = '<span class="cursor-pointer select-none relative inline-flex items-center justify-center inline-block p-4 px-5 py-3 overflow-hidden font-medium text-blue-500 rounded-lg shadow-2xl group"><span class="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-500 bg-blue-700 rounded-full blur-md ease"></span><span class="absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease"><span class="absolute bottom-0 left-0 w-24 h-24 -ml-10 bg-blue-500 rounded-full blur-md"></span><span class="absolute bottom-0 right-0 w-24 h-24 -mr-10 bg-blue-700 rounded-full blur-md"></span></span><span id="up-tx" class="font-semibold relative text-white">Read Again</span>'
-	orderedListElement.innerHTML = `<div class="collapsible" onclick="toggleContent()">${bbbtn}</div><div class="content-l" style="display: none;"><ol class="mt-4 space-y-1 list-decimal list-inside">${sentences.map(sentence => `<li class="mb-2 text-left">${sentence}</li>`).join("")}</ol></div>`;
+	document.getElementById("orderedList").innerHTML = `<div class="collapsible" onclick="toggleContent()">${bbbtn}</div><div class="content-l" style="display: none;"><ol class="mt-4 space-y-1 list-decimal list-inside">${sentences.map(sentence => `<li class="mb-2 text-left">${sentence}</li>`).join("")}</ol></div>`;
 }
 
 function startTypingNextSentence() {
@@ -75,28 +102,7 @@ shuffleArray(sentences);
 
 startTypingNextSentence();
 
-var contentVisible = false;
-
-function toggleContent() {
-	var content = document.querySelector('.content-l');
-	var upTx = document.getElementById('up-tx');
-	if (content.style.display === 'none') {
-		content.style.display = 'block';
-		upTx.textContent = 'Hide';
-	} else {
-		content.style.display = 'none';
-		upTx.textContent = 'Read Again';
-	}
-	if (content) {
-		content.style.display = contentVisible ? 'none' : 'block';
-		contentVisible = !contentVisible;
-	}
-}
-
-const currentYear = new Date().getFullYear();
-const yearElement = document.getElementById("year");
-yearElement.textContent = currentYear.toString();
-const errorMessage = document.getElementById('errmsg');
+document.getElementById("year").textContent = new Date().getFullYear().toString();
 
 var sourceEditor = CodeMirror.fromTextArea(document.getElementById("source"), {
 	mode: "python",
@@ -114,20 +120,18 @@ var sourceEditor = CodeMirror.fromTextArea(document.getElementById("source"), {
 	}
 });
 
-var words = [
-	'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif',
-	'else', 'except', 'False', 'finally', 'for', 'from', 'global', 'if', 'import',
-	'in', 'is', 'lambda', 'None', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return',
-	'True', 'try', 'while', 'with', 'yield', 'abs', 'all', 'any', 'ascii', 'bin',
-	'bool', 'bytearray', 'bytes', 'callable', 'chr', 'classmethod', 'compile', 'complex',
-	'delattr', 'dict', 'dir', 'divmod', 'enumerate', 'eval', 'exec', 'filter', 'float',
-	'format', 'frozenset', 'getattr', 'globals', 'hasattr', 'hash', 'help', 'hex', 'id',
-	'input', 'int', 'isinstance', 'issubclass', 'iter', 'len', 'list', 'locals', 'map',
-	'max', 'memoryview', 'min', 'next', 'object', 'oct', 'open', 'ord', 'pow', 'print',
-	'property', 'range', 'repr', 'reversed', 'round', 'set', 'setattr', 'slice', 'sorted',
-	'staticmethod', 'str', 'sum', 'super', 'tuple', 'type', 'vars', 'zip', 'exec', 'Ellipsis',
-	'NotImplemented',
-]
+var minifiedEditor = CodeMirror.fromTextArea(document.getElementById("minified"), {
+	mode: "python",
+	theme: "mdn-like",
+	lineNumbers: true,
+	readOnly: true,
+	viewportMargin: 20,
+	scrollbarStyle: "overlay",
+	matchBrackets: true,
+	placeholder: "Minified code..."
+
+});
+
 CodeMirror.registerHelper("hint", "anyword", function(editor, options) {
 	var wordList = options.words || words;
 	var cur = editor.getCursor();
@@ -151,9 +155,8 @@ CodeMirror.registerHelper("hint", "anyword", function(editor, options) {
 });
 
 function setupFileInput() {
-	const dropArea = document.getElementById('dropArea');
-	const fileInput = document.getElementById('fileInput');
-	const fileNameDisplay = document.getElementById('fileNameDisplay');
+	const classlst = ['select-none', 'font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded'];
+	const classlst0 = ['select-none', 'font-bold', 'bg-green-500', 'text-white', 'py-1', 'px-2', 'rounded'];
 	const exctri1 = '<svg class="select-none inline font-bold w-4 h-6 m-1 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><style>svg{fill:#ffffff}</style><path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>';
 
 
@@ -163,7 +166,7 @@ function setupFileInput() {
 		if (selectedFile) {
 			if (selectedFile.name.toLowerCase().endsWith('.py')) {
 				fileNameDisplay.textContent = selectedFile.name;
-				fileNameDisplay.classList.add('font-bold', 'bg-green-500', 'text-white', 'py-1', 'px-2', 'rounded');
+				fileNameDisplay.classList.add(...classlst0);
 				handleFile(selectedFile);
 			} else {
 				generateButton.disabled = true;
@@ -194,14 +197,15 @@ function setupFileInput() {
 		const droppedFile = event.dataTransfer.files[0];
 
 		if (droppedFile) {
+			classNamesToAdd = [];
 			if (droppedFile.name.toLowerCase().endsWith('.py')) {
-				errorMessage.classList.remove('select-none', 'font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
+				errorMessage.classList.remove(...classlst);
 				fileNameDisplay.textContent = droppedFile.name;
-				fileNameDisplay.classList.add('select-none', 'font-bold', 'bg-green-500', 'text-white', 'py-1', 'px-2', 'rounded');
+				fileNameDisplay.classList.add(...classlst0);
 				handleFile(droppedFile);
 			} else {
-				fileNameDisplay.classList.remove('select-none', 'font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
-				errorMessage.classList.add('select-none', 'font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
+				fileNameDisplay.classList.remove(...classlst);
+				errorMessage.classList.add(...classlst);
 				errorMessage.innerHTML = `${exctri1} Invalid file format. Please select a .py file.`;
 
 				fileNameDisplay.textContent = '';
@@ -222,8 +226,8 @@ function setupFileInput() {
 
 			reader.readAsText(file);
 		} else {
-			fileNameDisplay.classList.remove('select-none', 'font-bold', 'bg-green-500', 'text-white', 'py-1', 'px-2', 'rounded');
-			errorMessage.classList.add('select-none', 'font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
+			fileNameDisplay.classList.remove(...classlst0);
+			errorMessage.classList.add(...classlst);
 			errorMessage.innerHTML = `${exctri1} File size exceeds 2MB. Please select a smaller file.`;
 			fileNameDisplay.textContent = '';
 			fileInput.value = '';
@@ -234,15 +238,13 @@ function setupFileInput() {
 window.addEventListener('load', setupFileInput);
 
 function dw_py() {
-	var originalFileName = document.getElementById('fileNameDisplay').textContent || "default.py";
-	var modifiedFileName = originalFileName.replace(/\.[^/.]+$/, "") + "_min.py";
 	var blob = new Blob([minifiedEditor.getValue()], {
 		type: "text/x-python"
 	});
 	var dataUri = URL.createObjectURL(blob);
 	var downloadLink = document.createElement("a");
 	downloadLink.href = dataUri;
-	downloadLink.download = modifiedFileName;
+	downloadLink.download = (document.getElementById('fileNameDisplay').textContent || "default.py").replace(/\.[^/.]+$/, "") + "_min.py";
 	downloadLink.click();
 	URL.revokeObjectURL(dataUri);
 }
@@ -250,42 +252,38 @@ function dw_py() {
 document.getElementById('dw').addEventListener('click', dw_py);
 
 sourceEditor.on("change", function(editor) {
-	var codeTextarea = document.getElementById("source");
-	codeTextarea.value = editor.getValue();
+	document.getElementById("source").value = editor.getValue();
 });
 
 sourceEditor.on("change", function() {
 	updateLineCount();
 });
 
-function updateLineCount() {
-	var lineCount = sourceEditor.lineCount();
-	var text = sourceEditor.getValue();
-	var textSizeInBytes = new TextEncoder().encode(text).length;
+function toggleContent() {
+	var content = document.querySelector('.content-l');
+	var upTx = document.getElementById('up-tx');
+	if (content.style.display === 'none') {
+		content.style.display = 'block';
+		upTx.textContent = 'Hide';
+	} else {
+		content.style.display = 'none';
+		upTx.textContent = 'Read Again';
+	}
+	if (content) {
+		content.style.display = contentVisible ? 'none' : 'block';
+		contentVisible = !contentVisible;
+	}
+}
 
-	document.getElementById("line-count").textContent = `Line Count: ${lineCount}`;
-	document.getElementById("text-size").textContent = (textSizeInBytes / 1024).toFixed(3) + " kB";
+function updateLineCount() {
+	document.getElementById("line-count").textContent = `Line Count: ${sourceEditor.lineCount()}`;
+	document.getElementById("text-size").textContent = (new TextEncoder().encode(sourceEditor.getValue()).length / 1024).toFixed(3) + " kB";
 	errorMessage.classList.remove('select-none', 'font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
 	errorMessage.textContent = "";
 }
 
-var minifiedEditor = CodeMirror.fromTextArea(document.getElementById("minified"), {
-	mode: "python",
-	theme: "mdn-like",
-	lineNumbers: true,
-	readOnly: true,
-	viewportMargin: 20,
-	scrollbarStyle: "overlay",
-	matchBrackets: true,
-	placeholder: "Minified code..."
-
-});
-
-var lineCountDiv = document.getElementById("line-count-out");
-
 function updateLineCount_out() {
-	var lineCount_out = minifiedEditor.lineCount();
-	lineCountDiv.textContent = `Line Count: ${lineCount_out}`;
+	document.getElementById("line-count-out").textContent = `Line Count: ${minifiedEditor.lineCount()}`;
 }
 
 minifiedEditor.on("change", updateLineCount_out);
@@ -293,10 +291,6 @@ minifiedEditor.on("change", updateLineCount_out);
 updateLineCount_out();
 
 function initializeMinifier() {
-	const minifyButton = document.getElementById('minify');
-	const sourceTextArea = document.getElementById('source');
-	const copyButton = document.getElementById('copy');
-
 	const api_url = 'https://api.python-minifier.com/minify';
 
 	function build_query() {
@@ -330,15 +324,11 @@ function initializeMinifier() {
 		return query;
 	}
 
-	function optionsChange() {
-		minifiedEditor.setValue('');
-	}
-
 	async function copyClick() {
 		try {
 			await navigator.clipboard.writeText(minifiedEditor.getValue());
 		} catch (error) {
-			console.error('Copy failed:', error);
+			console.error(`Copy failed:, ${error}`);
 		}
 	}
 
@@ -347,7 +337,6 @@ function initializeMinifier() {
 		generateButton.disabled = false;
 
 		minifiedEditor.setValue('');
-		const minifiedSizeSpan = document.getElementById('minified-size');
 		const load = '<svg class="select-none inline font-bold w-4 h-4 mb-1 mr-1 text-white animate-spin" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#fff"><g fill="none" fill-rule="evenodd"><g transform="translate(1 1)" stroke-width="2"><circle stroke-opacity=".5" cx="18" cy="18" r="18"/><path d="M36 18c0-9.94-8.06-18-18-18"><animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="1s" repeatCount="indefinite"/></path></g></g></svg>';
 		minifiedSizeSpan.innerHTML = `${load} Loading....`;
 
@@ -357,7 +346,7 @@ function initializeMinifier() {
 				headers: {
 					'Content-Type': 'text/plain'
 				},
-				body: sourceTextArea.value
+				body: document.getElementById('source').value
 			});
 
 			const exctri = '<svg class="select-none inline font-bold w-4 h-6 mb-1 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><style>svg{fill:#ffffff}</style><path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>';
@@ -388,11 +377,6 @@ function initializeMinifier() {
 		minifyButton.disabled = false;
 	}
 
-	function updateSourceSize() {
-		const sourceSizeSpan = document.getElementById('text-size');
-		sourceSizeSpan.textContent = `${(sourceTextArea.value.length / 1024).toFixed(3)}  kB`;
-	}
-
 	minifyButton.addEventListener('click', minifyClick);
 	copyButton.addEventListener('click', copyClick);
 
@@ -406,77 +390,44 @@ function initializeMinifier() {
 	options.forEach(option => {
 		const element = document.getElementById(option);
 		if (element) {
-			element.addEventListener('change', optionsChange);
+			element.addEventListener('change', function() {
+				minifiedEditor.setValue('');
+			});
 		}
 	});
 
-	updateSourceSize();
 	minifyButton.disabled = false;
 }
 
-function clearSource() {
-	sourceEditor.setValue('');
-}
-
-const minifiedSizeElement = document.getElementById("minified-size");
-const generateButton = document.getElementById('dw');
-
-const clearButton = document.getElementById('rm');
-clearButton.addEventListener('click', clearSource);
-
-document.getElementById("rm").addEventListener("click", function() {
-
-	generateButton.disabled = true;
-
-	fileNameDisplay.classList.remove('select-none', 'font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
-
-	errorMessage.classList.remove('select-none', 'font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
-
-	errorMessage.textContent = '';
-
-	fileNameDisplay.textContent = '';
-
-	fileInput.value = '';
-
-	document.getElementById("source").textContent = "";
-
-	minifiedEditor.setValue("");
-
-	minifiedSizeElement.textContent = "0.000 kB";
-
-});
-
-const clearButton0 = document.getElementById('rm0');
-clearButton0.addEventListener('click', clearSource);
-
-document.getElementById("rm0").addEventListener("click", function() {
-
-	generateButton.disabled = true;
-
-	fileNameDisplay.classList.remove('select-none', 'font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
-
-	errorMessage.classList.remove('select-none', 'font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded');
-
-	errorMessage.textContent = '';
-
-	fileNameDisplay.textContent = '';
-
-	fileInput.value = '';
-
-	document.getElementById("source").textContent = "";
-
-	minifiedEditor.setValue("");
-
-	minifiedSizeElement.textContent = "0.000 kB";
-
-});
-
 initializeMinifier();
 
-var contentVisible1 = false;
+function clearSource() {
+	const classNamesToRemove = ['select-none', 'font-bold', 'bg-red-500', 'text-white', 'py-1', 'px-2', 'rounded'];
+
+	generateButton.disabled = true;
+
+	fileNameDisplay.classList.remove(...classNamesToRemove);
+	fileNameDisplay.textContent = '';
+
+	errorMessage.classList.remove(...classNamesToRemove);
+	errorMessage.textContent = '';
+
+	fileInput.value = '';
+
+	document.getElementById("source").textContent = "";
+
+	minifiedEditor.setValue("");
+
+	sourceEditor.setValue('');
+
+	minifiedSizeElement.textContent = "0.000 kB";
+}
+
+document.getElementById('rm').addEventListener('click', clearSource);
+
+document.getElementById('rm0').addEventListener('click', clearSource);
 
 function toggleContent1() {
-	var contentDiv = document.querySelector('.content-ll');
 	if (contentDiv.style.maxHeight) {
 		contentDiv.style.maxHeight = null;
 	} else {
@@ -485,14 +436,10 @@ function toggleContent1() {
 }
 
 function toggleContentFiles() {
-	var content = document.querySelector('.content-ll');
 	content.style.display = content.style.display === 'none' ? 'block' : 'none';
 }
 
 function tickAllAndSetGlobals() {
-	var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-	var preservedGlobalsInput = document.getElementById('preserve_globals');
-
 	checkboxes.forEach(function(checkbox) {
 		checkbox.checked = true;
 	});
@@ -501,17 +448,13 @@ function tickAllAndSetGlobals() {
 }
 
 function resetOptions() {
-	var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-	var preservedGlobalsInput = document.getElementById('preserve_globals');
-	var otherTextFields = document.querySelectorAll('input[type="text"]:not(#preserve_globals)');
-
 	checkboxes.forEach(function(checkbox) {
 		checkbox.checked = false;
 	});
 
 	preservedGlobalsInput.value = 'handler';
 
-	otherTextFields.forEach(function(textField) {
+	document.querySelectorAll('input[type="text"]:not(#preserve_globals)').forEach(function(textField) {
 		textField.value = '';
 	});
 }
