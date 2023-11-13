4ccfc8d2b231d60ce5965e45aed220486455b873
Glad <120590157+glad432@users.noreply.github.com>
2023-11-13 14:03:06 +0530

    main

--- js/script.js
+++ js/script.js
@@ -159,6 +159,27 @@ function setupFileInput() {
 	const classlst0 = ['select-none', 'font-bold', 'bg-green-500', 'text-white', 'py-1', 'px-2', 'rounded'];
 	const exctri1 = '<svg class="select-none inline font-bold w-4 h-6 m-1 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><style>svg{fill:#ffffff}</style><path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>';
 
+	function dragpy() {
+		const droppedFile = event.dataTransfer.files[0];
+
+		if (droppedFile) {
+			classNamesToAdd = [];
+			if (droppedFile.name.toLowerCase().endsWith('.py')) {
+				errorMessage.classList.remove(...classlst);
+				fileNameDisplay.textContent = droppedFile.name;
+				fileNameDisplay.classList.add(...classlst0);
+				handleFile(droppedFile);
+			} else {
+				fileNameDisplay.classList.remove(...classlst);
+				errorMessage.classList.add(...classlst);
+				errorMessage.innerHTML = `${exctri1} Invalid file format. Please select a .py file.`;
+
+				fileNameDisplay.textContent = '';
+			}
+		} else {
+			fileNameDisplay.textContent = '';
+		}
+	}
 
 	fileInput.addEventListener('change', function(event) {
 		const selectedFile = event.target.files[0];
@@ -186,33 +207,17 @@ function setupFileInput() {
 
 	document.addEventListener('dragover', function(event) {
 		event.preventDefault();
+		dragpy()
 	});
 
 	document.addEventListener('drop', function(event) {
 		event.preventDefault();
+		dragpy()
 	});
 
 	dropArea.addEventListener('drop', function(event) {
 		event.preventDefault();
-		const droppedFile = event.dataTransfer.files[0];
-
-		if (droppedFile) {
-			classNamesToAdd = [];
-			if (droppedFile.name.toLowerCase().endsWith('.py')) {
-				errorMessage.classList.remove(...classlst);
-				fileNameDisplay.textContent = droppedFile.name;
-				fileNameDisplay.classList.add(...classlst0);
-				handleFile(droppedFile);
-			} else {
-				fileNameDisplay.classList.remove(...classlst);
-				errorMessage.classList.add(...classlst);
-				errorMessage.innerHTML = `${exctri1} Invalid file format. Please select a .py file.`;
-
-				fileNameDisplay.textContent = '';
-			}
-		} else {
-			fileNameDisplay.textContent = '';
-		}
+		dragpy()
 	});
 
 	function handleFile(file) {
