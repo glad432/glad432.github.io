d9af0886b6dacf2bd2e72e6562dd4896cd3e9b93
gladw-in <89470287+gladw-in@users.noreply.github.com>
2024-08-02 22:00:21 +0530

    main

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,21 @@
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-  base: 'https://glad432.github.io/'
+  base: 'https://glad432.github.io/',
+  plugins: [
+    createHtmlPlugin({
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleTypeAttributes: true,
+        minifyCSS: true,
+        minifyJS: true,
+      },
+    }),
+  ],
 });
